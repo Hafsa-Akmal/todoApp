de75d67169df5d6d924f03ec2878c0f1d5e55b6a
Hafsa Akmal <hafsaakmal003@gmail.com>
2026-01-14 16:47:30 +0500

    env port

--- backend2/src/app.js
+++ backend2/src/app.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/todos", todoRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
